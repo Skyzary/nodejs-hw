bd6ddb86f7f5b49aef4c802546cf6b0a2ad06412
Volodymyr Levkovskyi <132926053+Skyzary@users.noreply.github.com>
2026-04-16 16:12:26 +0200

    feat: update user avatar handling and improve password reset validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,12 +10,16 @@ export const updateUserAvatar = async (req, res, next) => {
 
     const result = await saveFileToCloudinary(req.file.buffer);
 
-    await User.findByIdAndUpdate(req.user._id, {
-      avatar: result.secure_url,
-    });
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        avatar: result.secure_url,
+      },
+      { new: true, returnDocument: 'after' },
+    );
 
     res.status(200).json({
-      url: result.secure_url,
+      url: user.avatar,
     });
   } catch (error) {
     next(error);
